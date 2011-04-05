620c53d8bb0fe0eb1779e42bae6e69bb916e51b7
kevin <kevinwuhoo@gmail.com>
2011-04-04 19:17:57 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YahooHacks::Application.config.secret_token = '024e8c5dc1f625aa3ba6d6411c7c56b4e0b56873d114468fd18ab19498871618c9f550ffee0b9de1bf7c833e3c2a30283fd04a73ca4bebf963abcf6d85ef2642'
